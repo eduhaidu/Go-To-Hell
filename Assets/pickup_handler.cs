5e96653d8fa436d6349443a19324f4d4830dc3c8
eduhaidu <edu.haidu@gmail.com>
2024-05-07 20:45:02 +0300

    Implemented sounds for pickups

--- Assets/pickup_handler.cs
+++ Assets/pickup_handler.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class pickup_handler : MonoBehaviour
 {
+    public Transform player;
     public GameObject weaponScript;
     public GameObject healthScript;
     // Start is called before the first frame update
+    public AudioClip healthPickupSound;
+    public AudioClip bulletPickupSound;
     void Start()
     {
-        
+        player = GameObject.Find("Q3Player").transform;
     }
 
     // Update is called once per frame
@@ -17,11 +20,13 @@ public class pickup_handler : MonoBehaviour
         if (other.tag=="BulletPickup"){
            var gunscr = weaponScript.GetComponent<shoot>();
             gunscr.bulletCount += other.gameObject.GetComponent<bullet_pickup>().bulletAmmount;
+            AudioSource.PlayClipAtPoint(bulletPickupSound,player.position);
             Destroy(other.gameObject);
         }
         if(other.tag=="HealthPickup"){
             var hpscr=healthScript.GetComponent<player_health>();
             hpscr.playerHealth+=other.gameObject.GetComponent<health_pickup>().healthAmmount;
+            AudioSource.PlayClipAtPoint(healthPickupSound,player.position);
             Destroy(other.gameObject);
         }
     }
