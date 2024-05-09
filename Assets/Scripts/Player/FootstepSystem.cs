54a2733ee4c8705cd5672ddc5c6e602252225c8d
Division13studio <division13official@gmail.com>
2024-05-09 21:31:35 +0300

    Fucked up footsteps even worse

--- Assets/Scripts/Player/FootstepSystem.cs
+++ Assets/Scripts/Player/FootstepSystem.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FootstepSystem : MonoBehaviour
@@ -11,32 +10,77 @@ public class FootstepSystem : MonoBehaviour
     public AudioClip wood;
     public AudioClip dirt;
 
-    RaycastHit hit;
-    public Transform RayStart;
+    public Transform rayStart;
     public float range;
     public LayerMask layerMask;
-    // Start is called before the first frame update
-    public IEnumerator checkFootstepTag(){
-        if(Physics.Raycast(RayStart.position,-RayStart.transform.up,out hit, range, layerMask)){
-            if(hit.collider.CompareTag("grass")) PlayFootstepSound(grass);
-            if(hit.collider.CompareTag("concrete")) PlayFootstepSound(concrete);
-            if(hit.collider.CompareTag("wood")) PlayFootstepSound(wood);
-            if(hit.collider.CompareTag("dirt")) PlayFootstepSound(dirt);
-            yield return new WaitForSeconds(.1f);
+
+    private bool isMoving;
+    private bool isPlayingFootstep;
+
+    private void Update()
+    {
+        CheckMovementInput();
+        Debug.DrawRay(rayStart.position, -rayStart.transform.up * range, Color.green);
+    }
+
+    private void CheckMovementInput()
+    {
+        if (!isMoving &&
+            (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) ||
+             Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)))
+        {
+            isMoving = true;
+            StartCoroutine(CheckFootstepTag());
+        }
+        else if (isMoving &&
+                 (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) ||
+                  Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)))
+        {
+            isMoving = false;
+            audioSource.Stop(); // Stop footstep sound when the player stops moving
         }
     }
 
-    void PlayFootstepSound(AudioClip floorType){
-        audioSource.pitch = Random.Range(0.8f,1f);
-        audioSource.PlayOneShot(floorType);
-    }
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CheckFootstepTag()
     {
-        if(Input.GetKey(KeyCode.W)) StartCoroutine(checkFootstepTag());
-        if(Input.GetKey(KeyCode.A)) StartCoroutine(checkFootstepTag());
-        if(Input.GetKey(KeyCode.S)) StartCoroutine(checkFootstepTag());
-        if(Input.GetKey(KeyCode.D)) StartCoroutine(checkFootstepTag());
-        Debug.DrawRay(RayStart.position, -RayStart.transform.up*range, Color.green);
+        isPlayingFootstep = true;
+        yield return new WaitForSeconds(0.1f); // Delay to ensure the player is moving
+
+        while (isMoving)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(rayStart.position, -rayStart.transform.up, out hit, range, layerMask))
+            {
+                AudioClip footstepSound = null;
+                switch (hit.collider.tag)
+                {
+                    case "concrete":
+                        footstepSound = concrete;
+                        break;
+                    case "grass":
+                        footstepSound = grass;
+                        break;
+                    case "wood":
+                        footstepSound = wood;
+                        break;
+                    case "dirt":
+                        footstepSound = dirt;
+                        break;
+                }
+
+                if (footstepSound != null)
+                    PlayFootstepSound(footstepSound);
+            }
+
+            yield return new WaitForSeconds(0.5f); // Adjust as needed
+        }
+
+        isPlayingFootstep = false;
+    }
+
+    private void PlayFootstepSound(AudioClip floorType)
+    {
+        audioSource.pitch = Random.Range(0.8f, 1f);
+        audioSource.PlayOneShot(floorType);
     }
 }
