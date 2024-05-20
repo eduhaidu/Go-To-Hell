ae21ae52125c70b5e00c108ece6f6191c1486f77
Division13studio <division13official@gmail.com>
2024-05-20 20:44:46 +0300

    Fixed footsteps and Rockets ignoring Collisions
    -Reduced rocket speed from 35 to 25 units
    -Set prefab rigidbody detection to continous
    -Fixed footsteps code so they do not loop if WASD are spammed or double pressed

--- Assets/Scripts/Player/FootStepsFinal.cs
+++ Assets/Scripts/Player/FootStepsFinal.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -7,68 +6,67 @@ public class FootStepsFinal : MonoBehaviour
 {
     public string[] validTags;
     public bool Grounded;
-
     public bool isWalking;
-
-    public bool stepsound; //debug only
+    public bool stepsound; // Debug only
 
     private bool isStepping = false;
-
     public AudioSource source;
-
     public AudioClip StepSound;
-
     public float stepInterval = 0.5f;
 
     private void OnTriggerEnter(Collider other)
     {
-
-        // Collision detected with a valid tag! Perform your desired actions here
-        Debug.Log("Collision detected with tag: " + other.gameObject.tag);
-        // ... (Your specific logic for handling the collision)
-        Grounded = true;
-        print("Walking on " + other.gameObject.tag);
+        if (validTags.Contains(other.gameObject.tag))
+        {
+            Debug.Log("Collision detected with tag: " + other.gameObject.tag);
+            Grounded = true;
+            print("Walking on " + other.gameObject.tag);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Grounded = false;
-        isWalking = false;
+        if (validTags.Contains(other.gameObject.tag))
+        {
+            Grounded = false;
+            isWalking = false;
+        }
     }
 
     void Update()
     {
         bool wasWalking = isWalking;
-        if(Grounded){
-            isWalking = Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S);
+        if (Grounded)
+        {
+            isWalking = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S);
         }
-        else{
+        else
+        {
             isWalking = false;
         }
-        if (isWalking && Grounded && !wasWalking){
-            stepsound=true;
-            StartCoroutine(ExecuteSteps());
+
+        if (isWalking && Grounded && !wasWalking)
+        {
+            stepsound = true;
+            if (!isStepping)
+            {
+                StartCoroutine(ExecuteSteps());
+            }
         }
-        else if (!isWalking&&isStepping){
+        else if (!isWalking && isStepping)
+        {
             stepsound = false;
-            //Stop any currently running footstep coroutine
-            StopCoroutine(ExecuteSteps());
-            isStepping = false;
         }
     }
 
-    IEnumerator ExecuteSteps(){
-        while(isWalking&&Grounded){
-            if(!isStepping){
-                isStepping = true;
-                source.PlayOneShot(StepSound,1.0f);
-                yield return new WaitForSeconds(stepInterval);
-                isStepping = false;
-            }
-            else{
-                yield return null;
-            }
+    IEnumerator ExecuteSteps()
+    {
+        isStepping = true;
+        while (isWalking && Grounded)
+        {
+            source.PlayOneShot(StepSound, 1.0f);
+            yield return new WaitForSeconds(stepInterval);
         }
+        isStepping = false;
     }
 }
-
