fb310b3e45f2dc996ed745b8ec4015bdfd77ee20
eduhaidu <edu.haidu@gmail.com>
2024-05-24 20:11:31 +0300

    Made Rocket deal less damage based on distance

--- Assets/ApplyDamageRadius.cs
+++ Assets/ApplyDamageRadius.cs
@@ -12,7 +12,11 @@ public class ApplyDamageRadius : MonoBehaviour
         if (other.gameObject.tag == "Enemy")
         {
             var enemy = other.gameObject.GetComponent<EnemyAI>();
-            enemy.health -= radiusDamage;
+            float distanceFromRocket = Vector3.Distance(this.transform.position,enemy.transform.position);
+            float maxDamageDistance = 5.0f;
+            float falloffFactor = Mathf.Clamp01(1-(distanceFromRocket/maxDamageDistance));
+            int damage = Mathf.RoundToInt(radiusDamage*falloffFactor);
+            enemy.health -= damage;
 
             this.gameObject.SetActive(false);
         }
