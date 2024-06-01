3810b28f310f9f74a91ce32f2e5448f7f228349d
Division13studio <division13official@gmail.com>
2024-06-02 00:00:48 +0300

    Fixed GoreModel Collider Disable

--- Assets/ManageModelPieces.cs
+++ Assets/ManageModelPieces.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class ManageModelPieces : MonoBehaviour
 {
     public GameObject[] modelPieces;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update(){
-        for(int i=0;i<modelPieces.Length;i++){
-            if(modelPieces[i].GetComponent<Rigidbody>().velocity.magnitude<=0.1){
-                foreach(Collider c in GetComponents<Collider>()){
-                    c.enabled=false;
+    void Update()
+    {
+        for (int i = 0; i < modelPieces.Length; i++)
+        {
+            Rigidbody rb = modelPieces[i].GetComponent<Rigidbody>();
+            if (rb != null && rb.velocity.magnitude <= 0.1f)
+            {
+                Collider[] colliders = modelPieces[i].GetComponents<Collider>();
+                foreach (Collider c in colliders)
+                {
+                    Destroy(c.GetComponent<Rigidbody>());
+                    c.enabled = false;
+                    Destroy(c);
                 }
             }
-            }
         }
+    }
 }
-
