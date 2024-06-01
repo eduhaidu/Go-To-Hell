6d9046ae09a16b0a757d1ed9e853a58a1ba01d06
eduhaidu <edu.haidu@gmail.com>
2024-06-01 22:28:17 +0300

    Attempted instantiating decal projector at runtime

---
+++ Assets/CreateDecal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CreateDecal : MonoBehaviour
+{
+    public GameObject m_DecalProjectorPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject m_DecalProjectorObject = Instantiate(m_DecalProjectorPrefab,this.gameObject.transform.position,quaternion.identity);
+        DecalProjector m_DecalProjectorComponent = m_DecalProjectorObject.GetComponent<DecalProjector>();
+
+        // Creates a new material instance for the DecalProjector.
+        m_DecalProjectorComponent.material = new Material(m_DecalProjectorComponent.material);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
