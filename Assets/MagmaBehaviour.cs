0cbd110a935f10e8e71d9ce06b7891c6813eb624
eduhaidu <edu.haidu@gmail.com>
2024-06-21 13:59:38 +0300

    Magma Projectiles can now hurt the player

--- Assets/MagmaBehaviour.cs
+++ Assets/MagmaBehaviour.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MagmaBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int MagmaDamage=50;
+    public bool hasExploded=false;
+    
+    private void OnCollisionEnter(Collision collision){
+        if(!hasExploded){
+            print("Collided with " + collision.transform.gameObject.name);
+            GameObject player = GameObject.Find("Q3Player");
+            if (collision.transform.gameObject.name=="Q3Player"){
+                player.GetComponent<player_health>().playerHealth-=MagmaDamage;
+                if(player.GetComponent<player_health>().playerHealth<=0){
+                    player.GetComponent<player_health>().PlayerDeath();
+                }    
+            }
+            hasExploded=false;
+            Destroy(this.gameObject);
+        }
     }
 }
