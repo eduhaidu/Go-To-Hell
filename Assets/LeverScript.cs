a5cf4e34acc793463324f14018ef3377a856e52c
eduhaidu <edu.haidu@gmail.com>
2024-07-17 18:08:47 +0300

    Improved lever functionality

--- Assets/LeverScript.cs
+++ Assets/LeverScript.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class LeverScript : MonoBehaviour, IInteractable
 {
+    public bool isActivated=false;
     public void Interact()
     {
+        isActivated=true;
         Debug.Log("Lever activated");
     }
 }
